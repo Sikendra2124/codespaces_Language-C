451c493e7f3fd5b10f48edb16515b76d1377db05
Sikendra Yadav <134275709+Sikendra2124@users.noreply.github.com>
2025-05-02 17:26:21 +0000

    program file updated

---
+++ Technical Training/basics mathematics program/accessing_values.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i, arr[5]={10,20,30,40,50};
+    printf("array elements are:");
+    for(i=0;i<=5;i++){
+        printf("%d\t",arr[i]);
+    }
+    return 0;
+}
